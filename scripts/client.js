cab60d3045aa37ff107fc0d1ccf9b1bc5871c84f
Lara Komosa <larakomosa@gmail.com>
2020-10-09 16:09:21 -0500

    employee object created and pushed to array

--- scripts/client.js
+++ scripts/client.js
@@ -6,4 +6,23 @@ $(document).ready(readyNow);
 
 function readyNow() {
   console.log('working');
+  $('.js-button').on('click', addEmployee);
+}
+
+function addEmployee() {
+  const firstName = $('.js-field-first').val();
+  const lastName = $('.js-field-last').val();
+  const idNumber = $('.js-field-id').val();
+  const jobTitle = $('.js-field-title').val();
+  const annualSalary = $('.js-field-salary').val();
+  console.log(firstName, lastName, idNumber, jobTitle, annualSalary);
+
+  const employee = {
+    firstName,
+    lastName,
+    idNumber,
+    jobTitle,
+    annualSalary,
+  };
+  employeeList.push(employee);
 }
